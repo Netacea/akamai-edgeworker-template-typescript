14535e4e863a234c7078cb183f571a445eddc066
Lee Martin <lee.martin@netacea.com>
2021-10-04 12:28:26 +0100

    feat(akamai): II-34: Adding Linting

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,15 +2,15 @@
 import * as test from "tape";
 import { hello } from "../src/main"
 
-test("Hello Default Test:: Returns Hello World!", (t) => {
-    t.equal(hello(), "Hello World!");
-    t.end();
+test("Hello Default Test:: Returns Hello World!", t => {
+  t.equal(hello(), "Hello World!");
+  t.end();
 });
 
-test("Hello Input Test:: Returns Hello {input}!", (t) => {
-    t.equal(hello("Friend"), "Hello Friend!");
-    t.equal(hello("Compadre"), "Hello Compadre!");
-    t.equal(hello("Comrade"), "Hello Comrade!");
-    t.equal(hello("Freund"), "Hello Freund!");
-    t.end();
+test("Hello Input Test:: Returns Hello {input}!", t => {
+  t.equal(hello("Friend"), "Hello Friend!");
+  t.equal(hello("Compadre"), "Hello Compadre!");
+  t.equal(hello("Comrade"), "Hello Comrade!");
+  t.equal(hello("Freund"), "Hello Freund!");
+  t.end();
 });
